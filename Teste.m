cbbe0a467c0c4c31ed4249b5264b290968a1b53c
Matheus Henrique Orsini <matheus1506200@outlook.com>
2021-02-24 16:17:30 -0300

    Aula 1

---
+++ Teste.m
@@ -0,0 +1,6 @@
+raio = input("Digite seu raio: ");
+area = 2 * pi * raio^2;
+circunferencia = 2*pi*raio;
+disp("Sua area e circunferencia são respectivamente: ");
+disp(area);
+disp(circunferencia);
