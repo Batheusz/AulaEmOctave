ef7faafe02f69f3cd44535bbbd9e5cbdcfcbfe4e
Matheus Henrique Orsini <matheus1506200@outlook.com>
2021-03-20 12:38:00 -0300

    Aula2

--- Teste.m
+++ Teste.m
@@ -1,6 +1,18 @@
-raio = input("Digite seu raio: ");
-area = 2 * pi * raio^2;
-circunferencia = 2*pi*raio;
-disp("Sua area e circunferencia são respectivamente: ");
-disp(area);
-disp(circunferencia);
+dia_atual = input("Digite o dia atual: ");
+mes_atual = input("Digite o mes atual: ");
+ano_atual = input("Digite o ano atual: ");
+dia_nasc = input("Digite o dia do seu nascimento: ");
+mes_nasc = input("Digite o mes do seu nascimento: ");
+ano_nasc = input("Digite o ano do seu nascimento: ");
+
+if (ano_atual-ano_nasc > 18)
+  disp("Voce eh maior de 18");
+elseif(ano_atual-ano_nasc == 18)
+  if(mes_atual>=mes_nasc && dia_atual>= dia_nasc)
+    disp("Voce eh maior de 18");
+  else
+    disp("Voce eh menor de 18");
+  end
+else
+  disp("Voce eh menor de 18");
+end
